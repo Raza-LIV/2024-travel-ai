3ef50886ca5bb6a5ffd923f7bfdce59a48711759
1vas1k <ivan.lyoskin@gmail.com>
2024-03-05 01:41:20 +0200

    ref: Refactored Home page

--- packages/frontend/src/pages/Home/HomeDesktop/HomeDesktop.styled.ts
+++ packages/frontend/src/pages/Home/HomeDesktop/HomeDesktop.styled.ts
@@ -1,33 +1,50 @@
 import { css } from "@emotion/css";
+import { COLORS } from "../../../constants/colors";
 
 export const container = css`
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-`
-// export const navbar = css`
-//     display: flex;
-//     flex-direction: column;
-//     justify-content: flex-start;
-//     height: 100%;
-//     background-color: yellow;
-//     transition: 0.3s;
-// `
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+`;
 export const content = css`
-    width: calc(100% - 70px);
-    height: 100%;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  transition-duration: 500ms;
+`;
 export const centerBlock = css`
-    height: 100%;
-    width: 900px;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: center;
-`
+  height: 100%;
+  width: 900px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`;
+export const greating = css`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 30px 0 0 0;
+  color: ${COLORS.PRIMARY};
+  font-size: 22px;
+`;
+export const userName = css`
+  font-weight: bold;
+`;
+export const menu = css`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: 20px 0 0 0;
+`;
+export const itemHolder = css`
+  width: 48%;
+  margin: 12px 0;
+`;
--- packages/frontend/src/pages/Home/HomeDesktop/HomeDesktop.tsx
+++ packages/frontend/src/pages/Home/HomeDesktop/HomeDesktop.tsx
@@ -1,8 +1,24 @@
 import React from "react";
-import { centerBlock, container, content } from "./HomeDesktop.styled";
+import {
+  centerBlock,
+  container,
+  content,
+  greating,
+  userName,
+  menu,
+  itemHolder,
+} from "./HomeDesktop.styled";
 import { css } from "@emotion/css";
 import { useNavbarActive } from "../../../store/isNavbarOpen";
 import { Navbar } from "../../../components/Navbar/Navbar";
+import { TEXT } from "../../../constants/text";
+import { MobileHomeItem } from "../../../components/MobileHomeItem/MobileHomeItem";
+import { GenerationIcon } from "../../../assets/icons/GenerationIcon";
+import { ICON_SIZE } from "../../../constants/iconSize";
+import { ROUTES } from "../../../constants/roures";
+import { SavedIcon } from "../../../assets/icons/SavedTravels";
+import { FavoritesIcon } from "../../../assets/icons/FavoritesIcon";
+import { SettingsIcon } from "../../../assets/icons/SettingsIcon";
 
 export const HomeDesktop = () => {
   const { isNavbarOpen, setIsNavbarOpen } = useNavbarActive();
@@ -12,10 +28,60 @@ export const HomeDesktop = () => {
       <div
         className={css(
           content,
-          `width: ${isNavbarOpen ? "calc(100% - 290px)" : "calc(100% - 70px)"}`
+
+          `padding-left: ${isNavbarOpen ? "290px" : "70px"}`
         )}
       >
-        <div className={centerBlock}></div>
+        <div className={centerBlock}>
+          <div className={greating}>
+            <p>{TEXT.WELCOME}</p>
+            <p className={userName}>{TEXT.USER}</p>
+          </div>
+          <div className={menu}>
+            <div className={itemHolder}>
+              <MobileHomeItem
+                icon={
+                  <GenerationIcon isPrimaryColor={false} size={ICON_SIZE[40]} />
+                }
+                description={TEXT.GENERATION_DESCRIPTION}
+                label={TEXT.GENERATION}
+                isPrimary={true}
+                nav={ROUTES.GENERATION}
+              />
+            </div>
+            <div className={itemHolder}>
+              <MobileHomeItem
+                icon={<SavedIcon isPrimaryColor={true} size={ICON_SIZE[40]} />}
+                description={TEXT.SAVED_DESCRIPTION}
+                label={TEXT.SAVED}
+                isPrimary={false}
+                nav={ROUTES.SAVED}
+              />
+            </div>
+            <div className={itemHolder}>
+              <MobileHomeItem
+                icon={
+                  <FavoritesIcon isPrimaryColor={true} size={ICON_SIZE[40]} />
+                }
+                description={TEXT.FAVORITES_DESCRIPTION}
+                label={TEXT.FAVORITES}
+                isPrimary={false}
+                nav={ROUTES.TRAVEL}
+              />
+            </div>
+            <div className={itemHolder}>
+              <MobileHomeItem
+                icon={
+                  <SettingsIcon isPrimaryColor={true} size={ICON_SIZE[40]} />
+                }
+                description={TEXT.SETTINGS_DESCRIPTION}
+                label={TEXT.SETTINGS}
+                isPrimary={false}
+                nav={ROUTES.SETTINGS}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
